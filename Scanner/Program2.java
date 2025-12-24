d3bb29d3e009f18349d3f3d376c3a3d87eb61654
priyanshi pal <palpriyanshi712@gmail.com>
2025-12-24 07:35:04 +0530

    Program2

--- Scanner/Program2.java
+++ Scanner/Program2.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
 public class Program2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Num 1 : ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter Num 2 : ");
-        int num2 = sc.nextInt();
-        int sum = num1+num2;
-        System.out.println("Sum of " + num1 + " and " + num2+ " = " + sum);
-        sc.close();
-    }
+   public Program2() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter Num 1 : ");
+      int num1 = sc.nextInt();
+      System.out.print("Enter Num 2 : ");
+      int num2 = sc.nextInt();
+      int sum = num1 + num2;
+      System.out.print("Sum of " + num1 + " and " + num2 + " = " + sum);
+      sc.close();
+   }
 }
