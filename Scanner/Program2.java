5e71e4924c827efe4a5cdacf03f2e048c51a1a9b
priyanshi pal <palpriyanshi712@gmail.com>
2025-12-24 07:43:51 +0530

    Case one of sting

--- Scanner/Program2.java
+++ Scanner/Program2.java
@@ -6,12 +6,18 @@ public class Program2 {
 
    public static void main(String[] var0) {
       Scanner sc = new Scanner(System.in);
-      System.out.print("Enter Num 1 : ");
-      int num1 = sc.nextInt();
-      System.out.print("Enter Num 2 : ");
-      int num2 = sc.nextInt();
-      int sum = num1 + num2;
-      System.out.print("Sum of " + num1 + " and " + num2 + " = " + sum);
+    //   System.out.print("Enter Num 1 : ");
+    //   int num1 = sc.nextInt();
+    //   System.out.print("Enter Num 2 : ");
+    //   int num2 = sc.nextInt();
+    //   int sum = num1 + num2;
+    //   System.out.print("Sum of " + num1 + " and " + num2 + " = " + sum);
+
+    // Case one
+    String name = sc.nextLine();
+    int age = sc.nextInt();
+    System.out.println("Enter Name : " + name);
+    System.out.println("Enter AGE : " + age);
       sc.close();
    }
 }
