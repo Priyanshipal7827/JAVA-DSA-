c5e188f979d29549ef436ce7f6885cf222d5a46e
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-23 12:40:14 +0530

    variable shadowing

--- constractors/Parameterized/Car.java
+++ constractors/Parameterized/Car.java
@@ -1,3 +1,22 @@
 package constractors.Parameterized;
 
+public class Car {
+    String name;
+    int hp;
     
+    Car(String name , int hp){
+        name = name;
+        hp = hp;
+
+    }
+    public static void test(){
+        System.out.println("Car test");
+    }
+
+    public void PrintDetails(){
+          System.out.println("Name of Car : " + name);
+          System.out.println("HP is : " + hp);                                                                                                                                    
+
+    }  
+    
+}
--- constractors/Parameterized/CarTest.java
+++ constractors/Parameterized/CarTest.java
@@ -1,5 +1,12 @@
 package constractors.Parameterized;
 
 public class CarTest {
-    
+    public static void main(String[] args) {
+        System.out.println("Main Method is Started");
+        Car.test();
+        Car c1 = new Car("BMW",   180);
+        c1.PrintDetails();
+
+
+    }
 }
