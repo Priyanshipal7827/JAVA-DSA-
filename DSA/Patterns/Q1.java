5ef612471f7c20b75e0f8fe82dc9b934cc799c5e
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-23 13:19:55 +0530

    create files and folders

---
+++ DSA/Patterns/Q1.java
@@ -0,0 +1,14 @@
+package DSA.Patterns;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int n = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.println(j);
+            }
+            
+        }
+    }
+
+}
