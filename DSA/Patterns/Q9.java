3231e2817b740757f7c862cdf38240a53e65c897
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-23 22:20:40 +0530

    Question 9 is completed

---
+++ DSA/Patterns/Q9.java
@@ -0,0 +1,21 @@
+package DSA.Patterns;
+
+public class Q9 {
+    public static void main(String[] args) {
+        printPattern(11);
+    }
+    public static void printPattern(int n) {
+        int mid = n/2+1;        
+        for(int i = 1;i<=n;i++){
+            for(int j = 1; j<=n; j++){
+                if(i==1||i==n||j==1||j==n||i==j||i+j == n+1||i==mid||j==mid){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
