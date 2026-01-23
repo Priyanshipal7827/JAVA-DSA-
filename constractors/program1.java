dea3be170927d00636053cf863a687f91269bc1f
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-23 11:42:31 +0530

    write a program of constructor

--- constractors/program1.java
+++ constractors/program1.java
@@ -1,7 +1,20 @@
 package constractors;
 
 public class program1 {
+    int x = 10;
+
+    public void setDeatils(int a) {
+        x = a;
+        System.out.println("value of x : " + x);
+    }
+
+    public int printDetails() {
+        return x;
+    }
+
     public static void main(String[] args) {
-        
+        program1 p1 = new program1();
+        p1.setDeatils(10);
+        p1.printDetails();
     }
 }
