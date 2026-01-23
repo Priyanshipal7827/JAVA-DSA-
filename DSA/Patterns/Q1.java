76f263e1ef814a0c4889972f8697fa98b2d0ff9c
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-23 19:14:12 +0530

    Question 1 is completed

--- DSA/Patterns/Q1.java
+++ DSA/Patterns/Q1.java
@@ -1,13 +1,17 @@
 package DSA.Patterns;
 
+
 public class Q1 {
     public static void main(String[] args) {
-        int n = 1;
+        printPattern(5);
+       
+    }  
+    public static void printPattern(int n){        
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.println(j);
+            for (int j = 1; j <= n; j++) {
+                System.out.print(i + " ");
             }
-            
+            System.out.println();
         }
     }
 
