a25c27f65dfe876412da15eeedd0640f856f6c78
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-23 12:18:45 +0530

    no argument constractor

--- constractors/LaptopTest.java
+++ constractors/LaptopTest.java
@@ -4,7 +4,7 @@ public class LaptopTest {
      public static void main(String[] args) {
       System.out.println("Main Method is Start");
       Laptop.test();
-
-         
+      Laptop l1 = new Laptop();
+      l1.start();         
      }
 }
