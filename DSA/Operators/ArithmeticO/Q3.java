25122311f7f308aab28dc887e0f02fa2c11e5ff0
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-24 23:19:58 +0530

    Q3 is completed

---
+++ DSA/Operators/ArithmeticO/Q3.java
@@ -0,0 +1,12 @@
+package DSA.Operators.ArithmeticO;
+
+public class Q3 {
+    public static void main(String[] args) {
+        int n = 578;
+        while (n>0) {
+            int digit = n%10;
+            System.out.println(digit);
+            n = n/10;
+        }
+    }
+}
