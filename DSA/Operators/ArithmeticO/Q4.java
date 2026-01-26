274b478592aec77ca0539345b22896308889f7ee
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-26 16:17:21 +0530

    Q4 is completed

---
+++ DSA/Operators/ArithmeticO/Q4.java
@@ -0,0 +1,14 @@
+package DSA.Operators.ArithmeticO;
+
+public class Q4 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
