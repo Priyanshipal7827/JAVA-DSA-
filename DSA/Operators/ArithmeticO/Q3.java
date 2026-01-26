058c135d18497645844e29c99a7a61bfccb1b35f
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-26 16:13:52 +0530

    Q3 is completed

--- DSA/Operators/ArithmeticO/Q3.java
+++ DSA/Operators/ArithmeticO/Q3.java
@@ -3,10 +3,14 @@ package DSA.Operators.ArithmeticO;
 public class Q3 {
     public static void main(String[] args) {
         int n = 578;
-        while (n>0) {
-            int digit = n%10;
-            System.out.println(digit);
-            n = n/10;
-        }
+        int n1 = n/100;
+        System.out.println(n1);
+        int n2 = (n/10)%10;
+        System.out.println(n2);
+        int n3 = (n%10);
+        System.out.println(n3);
     }
 }
+
+
+
