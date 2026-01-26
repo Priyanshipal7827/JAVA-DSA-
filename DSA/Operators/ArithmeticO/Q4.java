417a9768bce585b11e66d00ee00a3db91580ec3c
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-26 16:25:53 +0530

    Q4 is completed without using third variable

--- DSA/Operators/ArithmeticO/Q4.java
+++ DSA/Operators/ArithmeticO/Q4.java
@@ -4,11 +4,16 @@ public class Q4 {
     public static void main(String[] args) {
         int a = 20;
         int b = 30;
-        int c = a;
-        a = b;
-        b = c;
+        // int c = a; using third variable
+        // a = b;
+        // b = c;
+        // System.out.println(a);
+        // System.out.println(b); 
+        
+        a = a+b;
+        b = a-b;
+        a = a-b;
         System.out.println(a);
         System.out.println(b);
-        
     }
 }
