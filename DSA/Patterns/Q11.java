554ee3aa6c2c9db38d45da3927201abbbe016f44
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-26 18:54:13 +0530

    Q11 is completed

---
+++ DSA/Patterns/Q11.java
@@ -0,0 +1,27 @@
+package DSA.Patterns;
+
+public class Q11 {
+
+    public static void main(String[] args) {
+        printPattern(11);
+
+    }
+
+    public static void printPattern(int n) {
+        int mid = n / 2 + 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n || i == j || i + j == n + 1 || i == mid || j == mid) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n;i++) {
+            System.out.println("**");
+
+        }
+    }
+}
