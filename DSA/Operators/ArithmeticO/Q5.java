d4d40b7299b71c24d6db8a568fcaa36f2bbdba5c
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-26 18:03:43 +0530

    Q5 is

--- DSA/Operators/ArithmeticO/Q5.java
+++ DSA/Operators/ArithmeticO/Q5.java
@@ -2,6 +2,22 @@ package DSA.Operators.ArithmeticO;
 
 public class Q5 {
     public static void main(String[] args) {
+        int val1 = 10;
+        int val2 = 20;
+        int val3 = 30;
+        // int val4 = val1; swap number with using fourth variable
+        // val1 = val2;
+        // val2 = val3;
+        // val3 = val4;
+        // System.out.println(val1);   
+        // System.out.println(val2);   
+        // System.out.println(val3); 
+        val1 = val1+val2+val3;
+        val2 = val1-val2;
+        val3= val2-val3;
         
+        System.out.println(val1);
+        System.out.println(val2);
+        System.out.println(val3);       
     }
 }
