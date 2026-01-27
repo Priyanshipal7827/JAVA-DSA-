5ec96c542cb8a318a5455dc1a6516f57c7bb994e
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-27 23:32:53 +0530

    Q27 is completed

---
+++ DSA/Patterns/Q27.java
@@ -0,0 +1,16 @@
+package DSA.Patterns;
+
+public class Q27 {
+    public static void main(String[] args) {
+        printPattern(5);
+    }
+    public static void printPattern(int n) {
+        int num = 64+1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)num++ +" ");
+            }            
+            System.out.println();
+        }
+    }
+}
