887fed3d097cc65510a7ce6ee280c0cffe748aa2
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-29 22:42:13 +0530

    Q29 is completed

---
+++ DSA/Patterns/Q29.java
@@ -0,0 +1,19 @@
+package DSA.Patterns;
+
+public class Q29 {
+    public static void main(String[] args) {
+        printPattern(5);
+
+    }
+
+    public static void printPattern(int n) {
+        int num = 64 + 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) num + " ");
+            }
+            num++;
+            System.out.println();
+        }
+    }
+}
