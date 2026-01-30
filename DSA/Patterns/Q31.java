334d75eac8aa4c3e02f877bb90c61f2ffd5b20b4
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-30 12:11:50 +0530

    Q24 is completed

--- DSA/Patterns/Q31.java
+++ DSA/Patterns/Q31.java
@@ -1,18 +1,18 @@
 package DSA.Patterns;
 
-public class Q24{
+public class Q31 {
     public static void main(String[] args) {
         printPattern(5);
     }
+
     public static void printPattern(int n) {
-        int num = n*(n+1)/2;
+        int num = 64+1;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(num+"  " );
-                num--;              
+            for(int j=n;j>=i;j--){
+                System.out.print((char)num + " ");
             }
+            num++;
             System.out.println();
         }
     }
 }
-
