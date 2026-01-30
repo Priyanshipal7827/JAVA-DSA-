3a1270be7c4cfde1ae807832ddf8562a3ab314a3
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-30 23:10:49 +0530

    Q35 is completed

--- DSA/Patterns/Q25.java
+++ DSA/Patterns/Q25.java
@@ -9,9 +9,9 @@ public class Q25 {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(num +"  " );
-                num++;              
+                              
             }
-           
+           num +=1;
             System.out.println();
         }
     }
