68c5a4965967daed409a0c6ee8cf31a390782ba3
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-30 12:13:37 +0530

    Q28 is completed

--- DSA/Patterns/Q28.java
+++ DSA/Patterns/Q28.java
@@ -5,10 +5,10 @@ public class Q28 {
         printPattern(5);
     }
      public static void printPattern(int n) {
-        int num = 64+1;
+        int num = n*(n+1)/2 + 64;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print((char)num++ +" ");
+                System.out.print((char)num-- +" ");
             }
            
             System.out.println();
