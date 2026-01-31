be45ed7d175fcc63072a8b5550b6b8d08227c3de
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-31 22:56:34 +0530

    Q40 is completed

--- DSA/Patterns/Q40.java
+++ DSA/Patterns/Q40.java
@@ -7,17 +7,17 @@ public class Q40 {
     public static void printPattern(int n) {
         int space = n-1;
         int patternWidth = 1;
-        int num = 1;
+        // int num = 1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=space;j++){
                 System.out.print("  ");
             }
-            for(int j=1;j<=patternWidth;j++){
-                System.out.print(num +" ");
+            for(int j = patternWidth; j >= 1; j--){
+                System.out.print(j +" ");
             }
             space--;
             patternWidth++;
-            num--;
+            // num--;
             System.out.println();
 
         }
