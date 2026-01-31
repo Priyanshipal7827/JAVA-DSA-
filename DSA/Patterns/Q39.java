9697db7ded05d1b28c19f409c42ae34ee654bbc2
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-31 22:19:31 +0530

    Q39 is completed with correct logic

--- DSA/Patterns/Q39.java
+++ DSA/Patterns/Q39.java
@@ -6,17 +6,19 @@ public class Q39 {
     }
     public static void printPattern(int n) {
         int space = n-1;
-        // int num = n+1;
+
+        int num = n;
         int patternWidth = 1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=space;j++){
                 System.out.print("  ");
             }
             for (int j=1;j<=patternWidth;j++) {
-                System.out.print(i +" ");
+                System.out.print(num +" ");
             }
             space--;
             patternWidth++;
+            num--;
             System.out.println();
         }
     }
