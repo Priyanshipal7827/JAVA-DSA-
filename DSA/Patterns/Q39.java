aa13a56b2a72cac83e6d6a2ab0790b8e103b817f
priyanshi pal <palpriyanshi712@gmail.com>
2026-01-31 22:13:58 +0530

    Q39 is allmost completed but logic diffrent

---
+++ DSA/Patterns/Q39.java
@@ -0,0 +1,23 @@
+package DSA.Patterns;
+
+public class Q39 {
+    public static void main(String[] args) {
+        printPattern(7);
+    }
+    public static void printPattern(int n) {
+        int space = n-1;
+        // int num = n+1;
+        int patternWidth = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print("  ");
+            }
+            for (int j=1;j<=patternWidth;j++) {
+                System.out.print(i +" ");
+            }
+            space--;
+            patternWidth++;
+            System.out.println();
+        }
+    }
+}
