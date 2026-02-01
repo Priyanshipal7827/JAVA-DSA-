ec7fd35a1f9adb3bf249b813c428ec579027ce49
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-01 14:50:12 +0530

    Q6

---
+++ DSA/Operators/ArithmeticO/Q8.java
@@ -0,0 +1,9 @@
+package DSA.Operators.ArithmeticO;
+
+public class Q8 {
+    public static void main(String[] args){
+        int n = 12345;
+        int num  = n/10000;
+        System.out.println(num);
+    }
+}
