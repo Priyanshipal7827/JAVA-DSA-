6abe53b194d16b6ef052bf3b7c95105864aac3f0
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-01 14:57:32 +0530

    Q9 is completed

--- DSA/Operators/ArithmeticO/Q9.java
+++ DSA/Operators/ArithmeticO/Q9.java
@@ -2,7 +2,7 @@ package DSA.Operators.ArithmeticO;
 
 public class Q9 {
     public static void main(String[] args) {
-        int x = 5/2*2;
+        double x = 5/2*2;
         System.out.println(x);
     }
 }
