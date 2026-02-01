bf0719e66a580466bae8a10b611b2014f97d523c
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-01 14:42:12 +0530

    Q7 is completed

---
+++ DSA/Operators/ArithmeticO/Q7.java
@@ -0,0 +1,12 @@
+package DSA.Operators.ArithmeticO;
+
+public class Q7 {
+    public static void main(String[] args) {
+        int cp = 120;
+        int sp = 90;
+        int loss = cp-sp;
+        System.out.println("Loss is : " +loss);
+        double lossInPercent = (loss*100)/cp;
+        System.out.println("Loss in % : " + lossInPercent);
+    }
+}
