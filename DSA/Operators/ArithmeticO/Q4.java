c4c36c2d67a628c240fc46f8a093d342d8169b5f
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-01 14:27:07 +0530

    Q4 is complted

--- DSA/Operators/ArithmeticO/Q4.java
+++ DSA/Operators/ArithmeticO/Q4.java
@@ -4,18 +4,24 @@ public class Q4 {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        //with using third variable
+        // with using third variable
         int c = a; // for store value(a) in temp variable
         a = b; // assign value(b) in a
         b = c;// assign value(c) or value(a) in b
         // System.out.println(c); // temp variable
         System.out.println(a);
         System.out.println(b);
+
+
         // without using third variable
-        b = b - a; // 20-10= 10
-        a = a + b; // 10+10=20
-        System.out.println(a);
-        System.out.println(b);
+       a=10;
+       b=20;
+       a = a+b;// 10+20 = 30
+       b = a-b;// 30-20=10
+       a = a-b; //30-10=20
+       System.out.println(a);
+       System.out.println(b);
+
 
     }
 }
