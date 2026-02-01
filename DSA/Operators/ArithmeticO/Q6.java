5ab74315da1c9c44499906610c8c6fc747cc0166
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-01 14:38:13 +0530

    Q6 is completed

---
+++ DSA/Operators/ArithmeticO/Q6.java
@@ -0,0 +1,12 @@
+package DSA.Operators.ArithmeticO;
+
+public class Q6 {
+    public static void main(String[] args) {
+        int cp = 120;
+        int sp = 160;
+        int profit = sp-cp;
+        System.out.println("Profit is : " + profit);
+        double profintPercent = (profit*100)/cp;
+        System.out.println("Profit in % : " + profintPercent);
+    }   
+}
