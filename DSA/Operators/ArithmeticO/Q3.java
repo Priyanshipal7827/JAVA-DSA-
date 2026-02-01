7b07bf9edd189c8bc1eaf3259b3247447711c0be
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-01 12:38:14 +0530

    Q3 is completed of arithmetic operator

--- DSA/Operators/ArithmeticO/Q3.java
+++ DSA/Operators/ArithmeticO/Q3.java
@@ -3,12 +3,14 @@ package DSA.Operators.ArithmeticO;
 public class Q3 {
     public static void main(String[] args) {
         int n = 578;
-        int n1 = n/100;
-        System.out.println(n1);
-        int n2 = (n/10)%10;
-        System.out.println(n2);
-        int n3 = (n%10);
-        System.out.println(n3);
+        System.out.println(n/100);//5
+        n=n%100;
+        // System.out.println(n);  //78     helping n
+        System.out.println(n/10);//7
+        System.out.println(n%10);//8
+
+
+
     }
 }
 
