08c6382a8a68318f7d600eb7f50e4c0a8b54b52b
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-01 12:22:55 +0530

    Q2 is completed of arithmetic operator

--- DSA/Operators/ArithmeticO/Q2.java
+++ DSA/Operators/ArithmeticO/Q2.java
@@ -3,11 +3,11 @@ package DSA.Operators.ArithmeticO;
 public class Q2 {
     public static void main(String[] args) {
         int n = 578;
-        while (n>0) {
-            int digit = n%10;
-            System.out.println(digit);
-            n = n/10;
-        }     
-       
+        System.out.println(n % 10);// 8
+        n = n / 10;
+        System.out.println(n);// 57
+        System.out.println(n % 10);//7
+        System.out.println(n / 10);//5
+
     }
 }
