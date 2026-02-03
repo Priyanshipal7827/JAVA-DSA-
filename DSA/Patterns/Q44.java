e12e56bd7040e3fe142ed2546375fad6bfb06924
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-03 11:29:54 +0530

    Q44 is complted

---
+++ DSA/Patterns/Q44.java
@@ -0,0 +1,26 @@
+package DSA.Patterns;
+
+public class Q44 {
+    public static void main(String[] args) {
+        printPattern(5);
+    }
+    public static void printPattern(int n) {
+       
+        int space = n-1;
+        int patternWidth = 1;
+        for(int i=1;i<=n;i++){
+             int num = 65;
+            for(int j=1;j<=space;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=patternWidth;j++){
+                System.out.print((char)num+ " "); 
+                num++;               
+            }
+            
+            space--;
+            patternWidth++;
+            System.out.println();
+        }
+    }
+}
