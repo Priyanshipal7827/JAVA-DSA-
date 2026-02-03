b91763771d5297847f44c33ab4da90f1975e7d64
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-03 22:51:03 +0530

    Q55 is completed

--- DSA/Patterns/Q55.java
+++ DSA/Patterns/Q55.java
@@ -7,16 +7,16 @@ public class Q55 {
 
     public static void printPattern(int n) {
         int space = n-1;
-        // int patternWidth = 1;
+        int patternWidth = 1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=space;j++){
                 System.out.print("  ");
             }
-            for(int j=1;j<=i;j++){
-                System.out.print((i+j)%2 + "   ");
+            for(int j=1;j<=patternWidth;j++){
+                System.out.print(j%2 + "   ");
             }
             space--;
-            // patternWidth++;
+            patternWidth++;
             System.out.println();
         }
 
