168a7e0fdf02bcebd7dfd73b9cd6faa73fd5aa6e
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-03 11:31:26 +0530

    Q51 is completed

---
+++ DSA/Patterns/Q51.java
@@ -0,0 +1,22 @@
+package DSA.Patterns;
+
+public class Q51 {
+    public static void main(String[] args) {
+        printPattern(7);
+    }
+    public static void printPattern(int n) {
+        int space = n-1;
+        int patternWidth = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=patternWidth;j++){
+                System.out.print("*   ");
+            }
+            space--;
+            patternWidth++;
+            System.out.println();
+        }
+    }
+}
