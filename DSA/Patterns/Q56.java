d5c80e5c434d43e35b455b4334eea947a5c762cd
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-03 11:40:42 +0530

    Q56 is completed

---
+++ DSA/Patterns/Q56.java
@@ -0,0 +1,25 @@
+package DSA.Patterns;
+
+public class Q56 {
+    public static void main(String[] args) {
+        printPattern(5);
+    }
+
+    public static void printPattern(int n) {
+        int space = n - 1;
+        int num = 1;
+        int patternWidth = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= patternWidth; j++) {
+                System.out.print(num++ + "   ");
+            }
+            space--;
+
+            patternWidth++;
+            System.out.println();
+        }
+    }
+}
