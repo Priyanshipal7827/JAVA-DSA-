df90f2a539a696a5eb6458ee1f52016a8d9edc02
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-04 13:20:48 +0530

    Q 59 is completed

---
+++ DSA/Patterns/Q59.java
@@ -0,0 +1,22 @@
+package DSA.Patterns;
+
+public class Q59 {
+    public static void main(String[] args) {
+        printPattern(5);
+    }
+    public static void printPattern(int n) {
+        int space = n-1;
+       
+        int num = 5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print("  ");
+            }
+            for(int j=num;j>=num-i+1;j--){
+                System.out.print(j+ "   ");               
+            }
+            space--;            
+            System.out.println();
+        }
+    }
+}
