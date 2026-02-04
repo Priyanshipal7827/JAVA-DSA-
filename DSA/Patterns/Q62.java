228966b539a736996309cb948cb97fbcc0ab3bb4
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-05 00:36:15 +0530

    Q62 is completed

---
+++ DSA/Patterns/Q62.java
@@ -0,0 +1,26 @@
+package DSA.Patterns;
+
+public class Q62 {
+    public static void main(String[] args) {
+        printPattern(5);
+    }
+    public static void printPattern(int n) {
+        int space=n-1;
+        int patternWidht = 1;
+        // int num = 65;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=patternWidht;j++){
+                System.out.print((char)('A'+j-1) +"   ");
+              
+            }
+            space--;
+            patternWidht++;
+            //   num++;
+            System.out.println();
+        }
+        
+    }
+}
