ea06b473746d907a1f4167ea0cc7d26ee79978cc
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-11 20:58:06 +0530

    Q 68 completed

---
+++ DSA/Patterns/Q68.java
@@ -0,0 +1,22 @@
+package DSA.Patterns;
+
+public class Q68 {
+    public static void main(String[] args) {
+        printPattern(5);
+    }
+    public static void printPattern(int n) {
+        int space = n-1;
+        // int num = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print(i+" ");
+            }
+            space--;
+            // num++;
+            System.out.println();
+        }
+    }
+}
