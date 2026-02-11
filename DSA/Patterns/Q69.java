e90d47cf0ca0cb59cb949de8146ebbc23a7cece6
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-11 21:05:10 +0530

    Q69 is completed and main think is i%2 for print 1's and 0's in rows

---
+++ DSA/Patterns/Q69.java
@@ -0,0 +1,20 @@
+package DSA.Patterns;
+
+public class Q69 {
+    public static void main(String[] args) {
+        printPattern(5);
+    }
+    public static void printPattern(int n) {
+        int space =n-1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print(i%2 +" ");
+            }
+            space--;
+            System.out.println();
+        }
+    }
+}
