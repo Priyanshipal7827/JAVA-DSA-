dc10a5c9e197f50661da7071333796bc397c577d
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-12 19:31:47 +0530

    Q71 completed

--- DSA/Patterns/Q71.java
+++ DSA/Patterns/Q71.java
@@ -1,5 +1,22 @@
 package DSA.Patterns;
 
 public class Q71 {
-    
+   public static void main(String[] args) {
+    printPattern(5);
+   }
+   public static void printPattern(int n) {
+        int space = n-1; 
+        int num =1;       
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print(j+ " ");
+                num++;               
+            }
+            space--;           
+            System.out.println();
+        }
+   }
 }
