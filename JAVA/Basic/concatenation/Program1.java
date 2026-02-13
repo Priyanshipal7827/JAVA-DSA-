01c569bcc237fd5f464ab34e6fdbeec570011936
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-13 11:10:39 +0530

    learn  string concatention concept

---
+++ JAVA/Basic/concatenation/Program1.java
@@ -0,0 +1,21 @@
+public class Program1{
+    public static void main(String[] args){
+        int x = 64;
+        int y = 20;
+        System.out.println("Sum is : " + x+10+y);
+        System.out.println(x+10+y+ " is Sum");
+        System.out.println(x+10+y+ " is Sum");
+        // System.out.println("Value is : "+ x-y);// complied error 
+        System.out.println(x-y+ " is Value");
+        System.out.println("Value is : "+ (x+y));
+        System.out.println("Value is : "+ (x-y));
+        System.out.println("Value is : "+ x+y);
+        System.out.println("Value is : "+ x/y);
+        System.out.println("Value is : "+ x%y);
+        System.out.println(x+y*2+'d'+(x/2+10)-y+"abc"+x+y/2+'d'+10);
+
+
+
+
+    }
+}
