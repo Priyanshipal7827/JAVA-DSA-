adf76ae8e7d0cc2926667c006e9c4b72cfa555d9
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-14 19:30:59 +0530

    Q73 is completed

--- DSA/Patterns/Q73.java
+++ DSA/Patterns/Q73.java
@@ -1,8 +1,9 @@
 package DSA.Patterns;
 
-public class Q72 {
+public class Q73 {
     public static void main(String[] args) {
         printPattern(5);
+
     }
     public static void printPattern(int n) {
         int space = n-1;
@@ -11,13 +12,11 @@ public class Q72 {
             for(int j=1;j<=space;j++){
                 System.out.print("  ");
             }
-            for(int j=1;j<=2*i-1;j++){
-                System.out.print(j%2 +" ");
+            for(int j=2*i-1;j>=1;j--){
+                System.out.print(j + " ");                          
             }
             space--;
-           
             System.out.println();
-    
         }
     }
 }
