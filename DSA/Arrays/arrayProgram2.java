924328e897dcefb24a7bfe5b9e595beba613335a
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-15 21:45:02 +0530

    learn the concept of sort array with sort() methd

---
+++ DSA/Arrays/arrayProgram2.java
@@ -0,0 +1,11 @@
+package DSA.Arrays;
+import java.util.*;
+public class arrayProgram2 {
+    public static void main(String[] args) {
+        int[] a = {12,10,20,15,30};
+        //sort array
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        
+    }
+}
