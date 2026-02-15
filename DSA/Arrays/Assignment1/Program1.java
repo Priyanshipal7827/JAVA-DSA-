637fe090c9f0e5b3fa3f140b963d04a567ec109f
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-15 22:17:06 +0530

    Access all even index elements from array

---
+++ DSA/Arrays/Assignment1/Program1.java
@@ -0,0 +1,10 @@
+package DSA.Arrays.Assignment1;
+
+public class Program1 {
+    public static void main(String[] args) {
+        int[] arr = {12,10,20,15,30};
+        for(int i=0;i<arr.length;i+=2){
+            System.out.println("index : "+ i +" = "+arr[i]);
+        }
+    }
+}
