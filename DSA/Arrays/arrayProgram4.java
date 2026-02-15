178c1a308b737d50072892321bc105c73a636426
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-15 21:58:48 +0530

    find indexes by users

---
+++ DSA/Arrays/arrayProgram4.java
@@ -0,0 +1,17 @@
+package DSA.Arrays;
+import java.util.*;
+public class arrayProgram4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {12,10,20,15,30};
+        System.out.print("Enter element to search : ");
+        int element = sc.nextInt();
+        int index = Arrays.binarySearch(arr, element);
+        if(index>=0){
+            System.out.println(element +" is found at index : "+index);
+        }
+        else{
+            System.out.println(element +" is not found in the array but can fit at index : "+(-index -1));
+        }
+    }
+}
