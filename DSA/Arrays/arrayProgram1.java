f3fa29f3907c86d9328694e0a68be461e3a339e2
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-15 21:40:15 +0530

    array concept

---
+++ DSA/Arrays/arrayProgram1.java
@@ -0,0 +1,11 @@
+package DSA.Arrays;
+import java.util.*;
+public class arrayProgram1 {
+    public static void main(String[] args) {
+        int[] a = {12,10,20,15,30};
+        System.out.println(a);
+        System.out.println(a.hashCode());// for hashcode and indexing value
+        System.out.println(Arrays.toString(a));
+        System.out.println("length of array is : "+a.length);
+    }
+}
