ffa164052a7b23e0d0f84618edcd64460e59928f
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-16 19:42:04 +0530

    i learn escape charactors

---
+++ JAVA/Basic/escapeCharactor/Program1.java
@@ -0,0 +1,19 @@
+package JAVA.Basic.escapeCharactor;
+public class Program1 {
+    public static void main(String[] args) {
+        /*
+         * learn escape charactors
+         * 1)"
+         * 2)'
+         * 3)\t
+         * 4)\n
+         * 5)\\
+         */
+        System.out.println("\"mohan\" and \"sohan\"");
+        System.out.println('\'');
+        System.out.println("today is \tmonday.");
+        System.out.println("tommarrow is \ntuesday");
+        System.out.println("mohan and \\sohan\\ are here");
+
+    }
+}
