bcb28bbf1c2c47515893dcac1be53cf2f3973804
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-17 22:05:42 +0530

    Q1 is completed swap the two arrays

--- DSA/Arrays/Assignment1/Q1.java
+++ DSA/Arrays/Assignment1/Q1.java
@@ -1,5 +1,26 @@
 package DSA.Arrays.Assignment1;
 
+import java.util.Arrays;
+
 public class Q1 {
-    
+    public static void main(String[] args) {
+        int[] a = {10,20,30,40,50};
+        int[] b = {60,70,80,90,100};
+        System.out.println("Array a : "+ Arrays.toString(a));
+        System.out.println("Array b : "+ Arrays.toString(b));
+
+       int[] temp = a;
+       a=b;
+       b=temp;
+       System.out.println("array a : " + Arrays.toString(a));
+       System.out.println("array b : " + Arrays.toString(b));
+
+        
+    }
+    // public static void swapArray(int[] arr){
+    //     for(int i=0;i<arr.length;i++){
+    //         System.out.print(arr[i]);
+    //     }
+    // }
 }
+
