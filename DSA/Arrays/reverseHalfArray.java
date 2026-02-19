e19a6ee3a797b51946f77d143cc170050e739932
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-19 13:37:18 +0530

    code clearity

--- DSA/Arrays/reverseHalfArray.java
+++ DSA/Arrays/reverseHalfArray.java
@@ -4,14 +4,15 @@ import java.util.Arrays;
 
 public class reverseHalfArray {
     public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50,60,70,80,90,100};
-        System.out.println("Orignal Array "+ Arrays.toString(arr));
-        reverseHaft(arr, 0, arr.length/2-1);
-        reverseHaft(arr, arr.length/2, arr.length-1);
+        int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        System.out.println("Orignal Array " + Arrays.toString(arr));
+        reverseHaft(arr, 0, arr.length / 2 - 1);
+        reverseHaft(arr, arr.length / 2, arr.length - 1);
         System.out.print("Reversed : " + Arrays.toString(arr));
     }
-    public static void reverseHaft(int[] a,int start,int end){
-        while (start<end) {
+
+    public static void reverseHaft(int[] a, int start, int end) {
+        while (start < end) {
             int temp = a[end];
             a[end] = a[start];
             a[start] = temp;
