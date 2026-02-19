ff628dbbdb399b2fd2eae2fee72d5caf5eb1dc01
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-19 19:58:03 +0530

    reduce space

--- DSA/Arrays/reverseHalfArray.java
+++ DSA/Arrays/reverseHalfArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class reverseHalfArray {
     public static void main(String[] args) {
-        int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
         System.out.println("Orignal Array " + Arrays.toString(arr));
         reverseHaft(arr, 0, arr.length / 2 - 1);
         reverseHaft(arr, arr.length / 2, arr.length - 1);
