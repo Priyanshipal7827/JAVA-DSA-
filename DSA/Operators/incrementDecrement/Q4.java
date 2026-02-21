3888a56db3a9a8adec0b9dde8972d13139d2b6db
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-21 06:37:34 +0530

    operation on increment decrement ++a or a++ and solve some problems

---
+++ DSA/Operators/incrementDecrement/Q4.java
@@ -0,0 +1,11 @@
+public class Q4 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = a++;
+        b++;
+        int c = a++ + --b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
