115dc018dbd3317c91db1fa71a5bb54bbbea9f9e
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-23 11:30:26 +0530

    we are learn about class's and object

---
+++ JAVA/classObject/Program1.java
@@ -0,0 +1,7 @@
+package JAVA.classObject;
+
+public class Program1 {
+    public static void main(String[] args) {
+        System.out.println("In this program we learn class's and objects");
+    }
+}
