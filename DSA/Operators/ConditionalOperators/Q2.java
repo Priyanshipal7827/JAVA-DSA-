36d05b59a390288f91f9f485ae0ef96470948fd7
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-24 23:44:49 +0530

    Q1 is completed check num is prime or not by using conditional operator

---
+++ DSA/Operators/ConditionalOperators/Q2.java
@@ -0,0 +1,9 @@
+package DSA.Operators.ConditionalOperators;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int n=5;
+        String Primenum =(n%2==0)?("not prime"):("prime");
+        System.out.println(Primenum);
+    }
+}
