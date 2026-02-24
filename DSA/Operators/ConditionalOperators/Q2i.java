fc415907bd9fd3ba8134afa50caba7651f3bc365
priyanshi pal <palpriyanshi712@gmail.com>
2026-02-24 23:53:55 +0530

    thress digit +ve or not

---
+++ DSA/Operators/ConditionalOperators/Q2i.java
@@ -0,0 +1,9 @@
+package DSA.Operators.ConditionalOperators;
+
+public class Q2i {
+    public static void main(String[] args) {
+        int num = 66;
+        boolean result = (num>100 && num<999)?true:false;
+        System.out.println(result);
+    }
+}
