aa1e9f934ebe1e449ee88f7f4cdffa1b71100800
priyanshi pal <palpriyanshi712@gmail.com>
2026-03-13 06:30:18 +0530

    some changes

--- DSA/String/S1.java
+++ DSA/String/S1.java
@@ -4,14 +4,14 @@ public class S1 {
 
     public static void main(String[] args) {
         String s1 = "ad3cdd4csf8";
-        ShiftDigit(s1);
+        System.out.println(ShiftDigit(s1));
     }
 
     public static String ShiftDigit(String s) {
         String rev = "";
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c <= '0' && c >= '9') {
+            if (c >= '0' && c <= '9') {
                 rev = rev + c;
             } else {
                 rev = c + rev;
