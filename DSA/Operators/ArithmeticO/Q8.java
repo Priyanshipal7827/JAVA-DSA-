bb1987f7a3b0fa3a157265c174950f0b437573cd
priyanshi pal <palpriyanshi712@gmail.com>
2026-03-13 06:26:22 +0530

    today we are learn about String

--- DSA/Operators/ArithmeticO/Q8.java
+++ DSA/Operators/ArithmeticO/Q8.java
@@ -3,7 +3,8 @@ package DSA.Operators.ArithmeticO;
 public class Q8 {
     public static void main(String[] args){
         int n = 12345;
-        int num  = n/10000;
+        int num  = n-(n/10)*10;
         System.out.println(num);
+        System.out.println(n-(n/10));
     }
 }
---
+++ DSA/String/S1.java
@@ -0,0 +1,22 @@
+package DSA.String;
+
+public class S1 {
+
+    public static void main(String[] args) {
+        String s1 = "ad3cdd4csf8";
+        ShiftDigit(s1);
+    }
+
+    public static String ShiftDigit(String s) {
+        String rev = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c <= '0' && c >= '9') {
+                rev = rev + c;
+            } else {
+                rev = c + rev;
+            }
+        }
+        return rev;
+    }
+}
