270832a3b1ba4ec0601bcd5fe7a4b107dd64afba
priyanshi pal <palpriyanshi712@gmail.com>
2026-04-03 22:05:56 +0530

    linear search concept

---
+++ DSA/Arrays/LinearSearch.java
@@ -0,0 +1,21 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 40, 16, 45, 30,60};
+       int key = 30;
+       int index = linearSearach(arr, key);
+       if(key == -1){
+        System.out.println("Key is not found");        
+       }
+       else{
+        System.out.println("key at index : " + index);
+       }
+    }
+    public static int linearSearach(int[] arr, int key){
+        for(int i =1; i< arr.length; i++){
+            if(arr[i]==key){
+                return i;
+            }           
+        }
+        return -1;
+    }
+}
